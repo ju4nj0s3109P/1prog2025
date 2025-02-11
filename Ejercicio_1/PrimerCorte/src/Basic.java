4ddd4bf124943a89eafb8e4eef5973f2565c0c73
juan jose <juan.j.rengifo1@gmail.com>
2025-02-11 17:51:53 -0500

    ejemplos estructuras

---
+++ Ejercicio_1/PrimerCorte/src/Basic.java
@@ -0,0 +1,34 @@
+public class Basic {
+
+    //Encapsulamiento - TipoRetorno - Nombre _ (Parametros)
+    private void test(){
+        // Codigo
+
+        return ;
+    }
+    private void test2(){
+        int num = 1;
+        String text = "";
+        boolean logico = false;
+        double real = 0.0;
+    }
+    // Ejemplos condicionales
+    private void test3() {
+        // If Basico
+        if () {
+        } else {
+
+        }
+    }
+    //Ejemplos Ciclos
+    private void test4() {
+        // Ciclo for
+        for (int i = 0; i < 10; i++) {
+            // Codigo
+        }
+        // Ciclo Do-While
+        do {
+        }while (condicion);
+    }
+
+}
